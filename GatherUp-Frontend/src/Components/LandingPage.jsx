b5d72dc6311a1627761e003c5eb3d06a1f015e07
PiyushSahu <piyushsahuys@gmail.com>
2025-05-03 21:26:11 +0530

    Starting out

---
+++ GatherUp-Frontend/src/Components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+function LandingPage() {
+    return ( 
+        <div className="landing-page">
+            <Section/>
+            <Section/>
+            <Section/>
+            <Section/>
+        </div>
+     );
+}
+
+export default LandingPage;
---
+++ GatherUp-Frontend/src/Components/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Section(url) {
+    return ( 
+        <div className="section">
+            <img src={url} alt="#" />
+        </div>
+     );
+}
+
+export default Section;
