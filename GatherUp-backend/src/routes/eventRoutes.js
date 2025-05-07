ed491b1f510789e94f7d5331d866584808836d1c
PiyushSahu <piyushsahuys@gmail.com>
2025-05-07 21:54:23 +0530

    final commit of first version

--- GatherUp-backend/src/routes/eventRoutes.js
+++ GatherUp-backend/src/routes/eventRoutes.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { searchEvents, submitEvents, browseEvents } from "../controllers/eventController.js";
 const router = Router();
 
-router.route("/searchEvent").post(searchEvents);
-router.route("/browseEvent").post(browseEvents);
+router.route("/searchEvent").get(searchEvents);
+router.route("/browseEvent").get(browseEvents);
 router.route("/submitEvent").post(submitEvents);
 
 
